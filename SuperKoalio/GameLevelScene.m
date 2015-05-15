b73ddb486765037d39484f3d9208f089153465eb
Giau Ngo <giaunv@gmail.com>
2015-05-15 08:01:09 +0700

    Finding tiles surrounding player

--- SuperKoalio/GameLevelScene.m
+++ SuperKoalio/GameLevelScene.m
@@ -14,6 +14,7 @@
 
 @property (nonatomic, strong) JSTileMap *map;
 @property (nonatomic, strong) Player *player;
+@property (nonatomic, strong) TMXLayer *walls;
 @property (nonatomic, assign) NSTimeInterval previousUpdateTime;
 
 @end
@@ -36,6 +37,37 @@
   return [layerInfo tileGidAtCoord:coord];
 }
 
+-(void)checkForAndResolveCollisionsForLayer:(Player *)player forLayer:(TMXLayer *)layer{
+  // Represent the positions of the tiles that surround the Koala.
+  NSInteger indices[8] = {7, 1, 3, 5, 0, 2, 6, 8};
+  
+  for (NSUInteger i = 0; i < 8; i++) {
+    NSInteger tileIndex = indices[i];
+    
+    // Retrieve the CGRect(in points) that will trigger a collision with the Player.
+    CGRect playerRect = [player collisionBoundingBox];
+    
+    // Finds the tile coordinate of the player's position. From which we'll find the eight other tile coordinates for the surrounding tiles.
+    CGPoint playerCoord = [layer coordForPoint:player.position];
+
+    // Finds a tile coordinate that is around the player's position.
+    NSInteger tileColumn = tileIndex % 3;
+    NSInteger tileRow = tileIndex / 3;
+    CGPoint tileCoord = CGPointMake(playerCoord.x + (tileColumn - 1), playerCoord.y + (tileRow - 1));
+    
+    // Look up the GID value for the tile at the tile coordinate found based on the index.
+    NSInteger gid = [self tileGIDAtTileCoord:tileCoord forLayer:layer];
+    
+    // If the GID is 0, it means that for thay layer, there is no tile at that coordinate, it's blank space, and we don't need to test for a collision with blank space.
+    if (gid) {
+      // Get the CGRect for the found tile in points
+      CGRect tileRect = [self tileRectFromTileCoords:tileCoord];
+      NSLog(@"GID %ld, Tile Coord %@, Tile Rect %@, Player Rect %@", (long)gid, NSStringFromCGPoint(tileCoord), NSStringFromCGRect(tileRect), NSStringFromCGRect(playerRect));
+      // Collision resolution goes here
+    }
+  }
+}
+
 // Before the scene is rendered, every frame will get called to this method.
 -(void)update:(NSTimeInterval)currentTime{
   // delta is used to scale movement and other forces (like gravity) in order to achieve smooth, consistent animations.
@@ -51,6 +83,8 @@
   self.previousUpdateTime = currentTime;
   
   [self.player update:delta];
+  
+  [self checkForAndResolveCollisionsForLayer:self.player forLayer:self.walls];
 }
 
 -(id)initWithSize:(CGSize)size {
@@ -59,6 +93,8 @@
     self.backgroundColor = [SKColor colorWithRed:.4 green:.4 blue:.95 alpha:1.0];
     self.map = [JSTileMap mapNamed:@"level1.tmx"];
     [self addChild:self.map];
+    
+    self.walls = [self.map layerNamed:@"walls"];
     
     self.player = [[Player alloc] initWithImageNamed:@"koalio_stand"];
     self.player.position = CGPointMake(100, 50);
